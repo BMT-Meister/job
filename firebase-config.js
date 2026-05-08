86882eb925ff4ccc0e1515f321a50112a2d1b924
BMT-Meister <bmt2607@bmt.hs.kr>
2026-05-08 13:52:37 +0900

    인증번호 오류 수정

---
+++ firebase-config.js
@@ -0,0 +1,30 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAUr-P92bcy-lm6J-yzVucxh9gcGdU3nIA",
+  authDomain: "bmt-job-hub.firebaseapp.com",
+  databaseURL: "https://bmt-job-hub-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bmt-job-hub",
+  storageBucket: "bmt-job-hub.firebasestorage.app",
+  messagingSenderId: "1032141927564",
+  appId: "1:1032141927564:web:d7c35b1b7199f433b577a9",
+  measurementId: "G-PVHKTH7NSB"
+};
+
+const SCHOOL_NAME   = "부산기계공업고등학교";
+const ALLOWED_DOMAIN = "bmt.hs.kr";
+const MASTER_EMAIL  = "bmt2607@bmt.hs.kr";
+
+// EmailJS
+const EMAILJS_CONFIG = {
+  serviceId:  "service_52ofr8w",
+  templateId: "template_u3ts08q",
+  publicKey:  "Ezt3dzfWaP8CkhhdZ"
+};
+
+// 학과 목록 (전체 공통 사용)
+const DEPT_LIST = [
+  "기계시스템과",
+  "설비시스템과",
+  "스마트팩토리과(로봇)",
+  "스마트팩토리과(메카)",
+  "전기과"
+];
